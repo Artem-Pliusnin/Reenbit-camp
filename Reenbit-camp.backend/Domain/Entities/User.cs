0810afa27917d34aad3453e1f7bbc4b0ae32fd45
Artem-Pliusnin <artemplyusnin2005@gmail.com>
2025-12-03 15:39:39 +0200

    Created User entity

---
+++ Reenbit-camp.backend/Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+namespace Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string? Avatar { get; set; }
+
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+
+    public string? RefreshToken { get; set; }
+    
+    public DateTime? RefreshTokenExpiry { get; set; }
+}
---
+++ Reenbit-camp.backend/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,42 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.FirstName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(u => u.LastName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(u => u.Avatar)
+            .HasMaxLength(255);
+
+        builder.Property(u => u.Email)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder.Property(u => u.Password)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.Property(u => u.RefreshToken)
+            .HasMaxLength(255);
+
+        builder.Property(u => u.RefreshTokenExpiry);
+    }
+}

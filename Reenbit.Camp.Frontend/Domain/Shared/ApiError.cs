a55cc22fe76f8dca2a675e00d3c58a19c091fa8b
Artem-Pliusnin <artemplyusnin2005@gmail.com>
2025-12-11 14:16:15 +0200

    Added result pattern

--- Reenbit.Camp.Frontend/Domain/Shared/ApiError.cs
+++ Reenbit.Camp.Frontend/Domain/Shared/ApiError.cs
@@ -6,4 +6,12 @@ public class ApiError
     public string? Type { get; set; }
     public string? Detail { get; set; }
     public int? Status { get; set; }
+    
+    public static ApiError NullValue => new()
+    {
+        Title = "Null Value",
+        Type = "NullValue",
+        Detail = "Specified value is null",
+        Status = 400
+    };
 }

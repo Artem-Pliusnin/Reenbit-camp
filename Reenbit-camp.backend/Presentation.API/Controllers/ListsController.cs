4cd7852635856ede23d9fab69aeb06a1289dc236
Artem-Pliusnin <artemplyusnin2005@gmail.com>
2025-12-12 16:34:03 +0200

    Implemented list deleting logic and added endpoints

--- Reenbit-camp.backend/Presentation.API/Controllers/ListsController.cs
+++ Reenbit-camp.backend/Presentation.API/Controllers/ListsController.cs
@@ -1,6 +1,9 @@
 using System.Security.Claims;
 using Application.Boards.Commands.CreateBoard;
 using Application.Lists.Commands.CreateList;
+using Application.Lists.Commands.DeleteList;
+using Application.Lists.Commands.UpdateList;
+using Application.Lists.Queries;
 using Domain.DTOs.Boards;
 using Domain.DTOs.Lists;
 using Domain.Errors;
@@ -20,6 +23,23 @@ public class ListsController : ApiController
     public ListsController(ISender sender)
         : base(sender)
     {}
+    
+    [HttpGet("board/{id}")]
+    public async Task<IActionResult> GetByBoardAsync(
+        [FromRoute] int id,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetListsByBoardQuery(id);
+        
+        Result<List<ListDto>> result = await Sender.Send(query, cancellationToken);
+        
+        if (result.IsFailure)
+        {
+            return HandleFailure(result);
+        }
+        
+        return Ok(result.Value);
+    }
     
     [HttpPost]
     public async Task<IActionResult> CreateAsync(
@@ -45,5 +65,49 @@ public class ListsController : ApiController
         }
         
         return Ok(result.Value);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAsync(
+        [FromBody] UpdateListRequest request,
+        [FromRoute] int id,
+        CancellationToken cancellationToken)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null ||
+            !int.TryParse(userIdClaim.Value, out var userId))
+        {
+            return  HandleUnauthorized(
+                Result.Failure(UserErrors.UserUnauthorized));
+        }
+
+        var command = new UpdateListCommand(userId, id, request.Title);
+        
+        Result result = await Sender.Send(command, cancellationToken);
+        
+        if (result.IsFailure)
+        {
+            return HandleFailure(result);
+        }
+        
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(
+        [FromRoute] int id,
+        CancellationToken cancellationToken)
+    {
+        var command = new DeleteListCommand(id);
+        
+        Result result = await Sender.Send(command, cancellationToken);
+        
+        if (result.IsFailure)
+        {
+            return HandleFailure(result);
+        }
+        
+        return Ok();
     }
 }

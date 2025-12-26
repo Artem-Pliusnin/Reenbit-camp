153b0802d16308fe05da2b7475875cf1061f684c
Artem-Pliusnin <artemplyusnin2005@gmail.com>
2025-12-26 17:57:34 +0200

    Added user controller

---
+++ Reenbit-camp.backend/Presentation.API/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+using System.Security.Claims;
+using Application.Boards.Queries.GetUserBoards;
+using Application.Users.Queries.GetInviteSuggestionUsers;
+using Domain.DTOs.Boards;
+using Domain.DTOs.Shared;
+using Domain.DTOs.Users;
+using Domain.Errors;
+using Domain.Models;
+using Domain.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.API.Abstractions;
+using Presentation.API.Contracts.Users;
+
+namespace Presentation.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+public class UsersController : ApiController
+{
+    public UsersController(ISender sender)
+        : base(sender)
+    {}
+    
+    [HttpGet]
+    public async Task<IActionResult> GetInvitationSuggestionsAsync(
+        [FromQuery] GetInviteSuggestionRequest request,
+        CancellationToken cancellationToken)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null ||
+            !int.TryParse(userIdClaim.Value, out var userId))
+        {
+            return  HandleUnauthorized(
+                Result.Failure(UserErrors.UserUnauthorized));
+        }
+        
+        var query = new GetInviteSuggestionUsersQuery(
+            request.BoardId,
+            userId,
+            request.Query, 
+            request.Limit);
+        
+        Result<List<UserDto>> result = await Sender.Send(query, cancellationToken);
+        
+        if (result.IsFailure)
+        {
+            return HandleFailure(result);
+        }
+        
+        return Ok(result.Value);
+    }
+}
